8972ea02e778bb3a91913502916f69f9cbe59849
Kardi <kgland06@gmail.com>
2024-08-07 22:26:28 +0700

    pasing request body

--- server.js
+++ server.js
@@ -1,7 +1,45 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req,res)=> {
-    console.log(req.url, req.method, req.headers);
+    const url = req.url;
+    const method = req.method;
+
+    if(url === '/'){
+        res.setHeader('Content-Type', 'text/html');
+
+        res.write('<html>');
+        res.write('<head><title>Nyobain node</title></head>');
+        res.write('<body>');
+        res.write('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form>');
+        res.write('</body>');
+        res.write('</html>');
+        return res.end();
+    }else if(url === '/message' && method === 'POST'){
+        const body = [];
+        req.on('data',  (chunk)=>{
+            body.push(chunk);
+            console.log(chunk);
+        });
+        req.on('end',()=>{
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        });
+        res.statusCode = 302;   
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Nyobain node</title></head>');
+    res.write('<body>');
+    res.write('<h1>Hello from node JS server</h1>');
+    res.write('</body>');
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(3000);
