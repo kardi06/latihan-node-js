608418fde2824fa4f78599017c7ab010782551a8
Kardi <kgland06@gmail.com>
2024-08-22 23:06:47 +0700

    simple-routing

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
-const http = require('http');
 const express = require('express');
 
 // console.log(routes.someText);
 const app = express();
 
-app.use((req, res, next)=>{
+app.use('/add-product',(req, res, next)=>{
     console.log("In The middleware");
-    next(); // Allow the request to continue to the next middleware
+    res.send("<h1>Add Product Page</h1>");
 })
 
-app.use((req, res, next)=>{
+app.use('/',(req, res, next)=>{
     console.log("In The another middleware");
+    res.send("<h1>Welcome guysss</h1>");
 })
 // const server = http.createServer(routes.handler);
-const server = http.createServer(app);
+// const server = http.createServer(app);
 
-server.listen(3000);
+// server.listen(3000);
+app.listen(3000);
