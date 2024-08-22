cc2ed4ccee7db36d050ddd8161145f81b4ce7add
Kardi <kgland06@gmail.com>
2024-08-22 22:50:35 +0700

    middleware

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const http = require('http');
-const routes = require('./routes');
+const express = require('express');
 
 // console.log(routes.someText);
+const app = express();
 
+app.use((req, res, next)=>{
+    console.log("In The middleware");
+    next(); // Allow the request to continue to the next middleware
+})
+
+app.use((req, res, next)=>{
+    console.log("In The another middleware");
+})
 // const server = http.createServer(routes.handler);
-const server = http.createServer(routes);
+const server = http.createServer(app);
 
 server.listen(3000);
