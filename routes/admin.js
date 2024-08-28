a6a9407056efa6d54edd567a3e3c94451424d1e6
Kardi <kgland06@gmail.com>
2024-08-28 19:22:21 +0700

    serving html

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,20 @@
+const path = require('path');
+
 const express = require('express');
 const router = express.Router();
 
 //admin/add-product => get
 router.get('/add-product',(req, res, next)=>{
     console.log("In The middleware");
-    res.send("<form action='admin/add-product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button></form>");
+    // res.send("<form action='admin/add-product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button></form>");
+    res.sendFile(path.join(__dirname,'../','views', 'add-product.html'));
 })
 
 //admin/add-product => post
 router.post('/add-product',(req, res, )=>{
     console.log(res.body);
     res.redirect('/');
+    
 })
 
 module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const path = require('path');
 const router = express.Router();
 
 router.get('/',(req, res, next)=>{
-    console.log("In The another middleware");
-    res.send("<h1>Welcome guysss</h1>");
+    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+    // console.log("In The another middleware");
+    // res.send("<h1>Welcome guysss</h1>");
+
 })
 
 module.exports = router;
