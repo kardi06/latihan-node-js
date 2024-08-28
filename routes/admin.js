a62b0257c7acc571c47f121dc4afd8d9ac8a84b8
Kardi <kgland06@gmail.com>
2024-08-28 21:00:26 +0700

    styling

--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,15 @@
 const path = require('path');
-
+const rootDir = require('../utils/path');
 const express = require('express');
+
 const router = express.Router();
 
 //admin/add-product => get
 router.get('/add-product',(req, res, next)=>{
     console.log("In The middleware");
     // res.send("<form action='admin/add-product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button></form>");
-    res.sendFile(path.join(__dirname,'../','views', 'add-product.html'));
+    res.sendFile(path.join(rootDir,'views', 'add-product.html'));
+    // res.sendFile(path.join(__dirname,'..','views', 'add-product.html'));
 })
 
 //admin/add-product => post
