f2a5cc006737ce8b5bdb503883503bb191d89b23
Kardi <kgland06@gmail.com>
2024-08-30 23:22:43 +0700

    pug file

--- routes/shop.js
+++ routes/shop.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const path = require('path');
 const rootDir = require('../utils/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/',(req, res, next)=>{
     // res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
     // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-    res.render('shop');
+    const product = adminData.product;
+    res.render('shop',{prods:product, docTitle: 'Shop'});
 
 })
 
