3d9946ee321d38632ef3e6b476aab3470ffc2170
Kardi <kgland06@gmail.com>
2024-09-16 23:44:22 +0700

    edit modul using path to add data

--- models/product.js
+++ models/product.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 const product = [];
 
 module.exports = class Product {
@@ -6,7 +9,19 @@ module.exports = class Product {
     }
 
     save() {
-        product.push(this);
+        const p = path.join(path.dirname(require.main.filename), 'data', 'products.json');
+        // product.push(this);
+        fs.readFile(p, (err, fileContent) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            const products = JSON.parse(fileContent);
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err);
+            });
+        });
     }
 
     static fetchAll() {
