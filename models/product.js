d97a851c087131b646d6f5a0d663dfd0c69c4365
Kardi <kgland06@gmail.com>
2024-09-17 21:51:36 +0700

    refactoring

--- models/product.js
+++ models/product.js
@@ -1,7 +1,16 @@
 const fs = require('fs');
 const path = require('path');
 
-const product = [];
+const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
+
+const getProductFromFile = (cb) => {   
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            cb([]);
+        }
+        cb(JSON.parse(fileContent));
+    });
+};
 
 module.exports = class Product {
     constructor(t) {
@@ -9,27 +18,30 @@ module.exports = class Product {
     }
 
     save() {
-        const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
-        // product.push(this);
-        fs.readFile(p, (err, fileContent) => {
-            let products = [];
-            if (!err) {
-                products = JSON.parse(fileContent);
-            }
+        getProductFromFile(products => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), (err) => {
                 console.log(err);
             });
         });
+        // const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
+        // // product.push(this);
+        // fs.readFile(p, (err, fileContent) => {
+        //     let products = [];
+        //     if (!err) {
+        //         products = JSON.parse(fileContent);
+        //     }
+            
+        // });
     }
 
     static fetchAll(cb) {
         const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
         fs.readFile(p, (err, fileContent) => {
             if (err) {
-                return cb([]);
+                cb([]);
             }
-            return cb(JSON.parse(fileContent));
+            cb(JSON.parse(fileContent));
         });
     }
 }
