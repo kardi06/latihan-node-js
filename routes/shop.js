f126e4eeadedec882f8cbe4f2e74c91603097651
Kardi <kgland06@gmail.com>
2024-10-03 23:34:32 +0700

    orders page

--- routes/shop.js
+++ routes/shop.js
@@ -10,6 +10,8 @@ router.get('/products', shopController.getProduct);
 
 router.get('/cart', shopController.getCart);
 
+router.get('/orders', shopController.getOrders);
+
 router.get('/checkout', shopController.getCart);
 
 module.exports = router;
